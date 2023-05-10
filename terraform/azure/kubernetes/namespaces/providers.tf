c2a35cd66dde287adbb91dbd4552326f9d47924a
root <root@DESKTOP-2JOUJ3A.localdomain>
2023-05-10 11:49:21 +0200

    Added requiered providers blocks

--- terraform/azure/kubernetes/namespaces/providers.tf
+++ terraform/azure/kubernetes/namespaces/providers.tf
@@ -1,7 +1,14 @@
-provider "azurerm" {
-    features {
-      
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.54.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"      
+    }
+  }
 }
 
 provider "kubernetes" {
